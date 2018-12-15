3db8f44e66c80a121b1b9553845084f0f7cdb5bd
sskarmanova <sskarmanova@yandex.ru>
2018-12-15 18:11:36 +0300

    second lesson

---
+++ slider-app/src/components/Main.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import Row from  'react-bootstrap/lib/Row';
+import Col from  'react-bootstrap/lib/Col';
+import styled from 'styled-components'; 
+
+const Repair = styled.div`
+  color:  #ffffff;
+  font-size: 40px;
+  font-weight: 700;
+  line-height: 50px;
+  
+  span {
+    display: block;
+    font-size: 24px;
+    line-height: 30px;
+  }
+`
+
+const LightText = styled.div`
+  color:  #ffffff;
+  font-size: 18px;
+  font-weight: 300;
+  line-height: 28px;
+  margin-top: 54px;
+`
+
+const CallButton = styled.button`
+  border-radius: 30px;
+  background-color:  #ffa14b;
+  width: 245px;
+  height: 64px;
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 32px;
+`
+
+class Main extends React.Component {
+  render(){
+    return (
+        <Row>
+          <Col lg={5}>
+            <Repair>
+              Качественный ремонт
+              <span> iphone за 35 минут и гарантия 1 год </span>
+            </Repair>
+            <LightText> 
+              Оставьте заявку на бесплатную диагностику без очереди и получите 
+              защитное стекло, стоимостью 1000 рублей, с утановкой в подарок! 
+            </LightText>
+            <CallButton> Отправить заявку! </CallButton>
+          </Col>
+          <Col lg={6} lgOffset={1}>
+
+          </Col>
+
+        </Row>
+      )
+  }
+}
+
+export default Main;
